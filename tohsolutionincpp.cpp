f8c206b2e4359adb5da7f9d2168088f1dafe207e
Divya0319 <divygupta0319@gmail.com>
2020-07-09 23:31:22 +0530

    C++ program- TOH problem

---
+++ tohsolutionincpp.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+	void toh(int n, char x, char y, char z, int& invocations){
+		invocations++;
+		if(n>1)
+		{
+			toh(n-1, x,z,y,invocations);
+			printf("\nMove top of %c to %c using %c", x,y,z);
+			toh(n-1, z,y,x,invocations);
+		}
+		else if(n==1) printf("\nMove top of %c to %c using %c",x,y,z);	
+	}
+	int main(){
+	int n;
+	int invocations=0;
+	printf("Enter number of discs:");
+	scanf("%d", &n);
+	toh(n,'x','y','z',invocations);
+	printf("\nNumber of invocations of toh()=%d\n", invocations);
+	return 0;
+	}
